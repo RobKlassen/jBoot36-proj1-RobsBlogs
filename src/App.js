7f0a64a4293646fbc902ce15eb4a12277060e97f
Rob <klassen.rob@gmail.com>
2021-10-02 17:26:06 -0400

    react hard

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import database from './firebase';
 function App() {
     
     const [userInput, setUserInput] = useState("");
-
     const [sectionList, setSectionList] = useState([]);
+    const [blogPosts, setBlogPosts] = useState([]);
 
 
     useEffect(function(){
         const dbRef = ref(database);
-        // const blogSections = [];
-        
+   
         onValue(dbRef, function(dbCurrent){
             const myData = dbCurrent.val();
+
             const dataObjectArray = [];
+            const dataBlogPostArray = [];
 
             for (let dbSection in myData){
                 const dataObject = {
@@ -26,10 +27,24 @@ function App() {
                 dataObjectArray.push(dataObject);
             }
             setSectionList(dataObjectArray);
-        })
+
+            for (let dbBlogPosts in myData.blogs){
+                const dataObject = {
+                    key: dbBlogPosts,
+                    title: myData.blogs[dbBlogPosts].title,
+                    content: myData.blogs[dbBlogPosts].content
+                }
+                dataBlogPostArray.push(dataObject);
+            }
+            setBlogPosts(dataBlogPostArray);
+
+            
+ 
+        });
 
     },[]);
-    // console.log(sectionList);
+    // console.log("section list", sectionList);
+    console.log("blog posts", blogPosts.title);
 
 
 
@@ -42,20 +57,47 @@ function App() {
         setUserInput(event.target.value);
     }
 
+    const turnObjectIntoArray = function(object){
+        const toArray = [];
+        for (let item in object){
+            toArray.push(item);
+        }
+        return toArray;
+    }
 
 
-    console.log("hello world");
 
     return (
         <div className="App">
             <header>
-                <h1>Robs Blog</h1>
+                <h1>Robs Blogss</h1>
                 <ul>
-                    {// get title data from firebase
-                        //map elements
-                            //get post from firebase title 
-                                //map those elements
-                    }    
+                    {
+                        sectionList.map(function(siteSection){
+                            return(
+                                <li key={siteSection.key}>
+                                    <p>{siteSection.key}</p>
+
+                                    {/* <p>{turnObjectIntoArray(siteSection.section)}</p> */}
+                                    {/* <p>{turnObjectIntoArray(siteSection.section.post1)}</p> */}
+                                      {/* <p>{(siteSection.section.post1.title)}</p> */}
+                                    {/* <p>{turnObjectIntoArray(siteSection.section[post1])}</p> */}
+                                    {/* <p>{section}</p>     */}
+                                </li>
+                            )
+                        })
+                    }
+                    {
+                        blogPosts.map(function(blogpost){
+                            return(
+                                <li key={blogpost.key}>
+                                    <p>{blogpost.key}</p>
+                                    <p>{blogpost.title}</p>
+                                    <p>{blogpost.content}</p>
+                                </li>    
+                            )
+                        })
+                    }
                 </ul>
                 <button>get some data from firebase</button>
 
@@ -83,6 +125,116 @@ function App() {
 }
 
 export default App;
+
+
+
+//  DECOMPILING DATA INTO SERIES OF NESTED ARRAYS
+
+// useEffect(function(){
+//     const dbRef = ref(database);
+//     const blogSections = [];
+    
+//     onValue(dbRef, function(dbCurrent){
+//         const myData = dbCurrent.val();
+        
+//         for (let dbSection in myData){
+//             const blogPosts = [];
+            
+//             for (let dbPosts in myData[dbSection]){
+//                 const postInfo = [];
+                
+//                 for (let dbParent in myData[dbSection][dbPosts]){
+//                     postInfo.push(myData[dbSection][dbPosts][dbParent]);
+//                 }
+//                 blogPosts.push(postInfo);
+//             }
+//             blogSections.push(blogPosts);
+//         }
+//         console.log(blogSections);
+//         console.log(blogSections[0]);
+//         console.log(blogSections[0][0]);
+//     })
+// },[]);
+
+
+
+
+// import { useState, useEffect } from 'react';
+// import realtime from './firebase';
+// import { ref, onValue, push } from 'firebase/database';
+// import './App.css';
+// // import { firebase/database } from 'firebase';
+
+
+// function App() {
+//     const [bookList, setBookList] = useState([]);
+//     const [userInput, setUserInput] = useState("");
+    
+//     useEffect( ()=> {
+//         const dbRef = ref(realtime);
+        
+//         onValue(dbRef, function(snapshot){
+//             const myData = snapshot.val();
+//             const newArray = [];
+            
+//             for (let propertyName in myData){
+//                 const bookObject = {
+//                     key: propertyName,
+//                     title: myData[propertyName]
+//                 }
+//                 newArray.push(bookObject);
+//             }
+//             setBookList(newArray);
+//         });
+//     },[]);
+
+//     const handleChange = function(event){
+//         setUserInput(event.target.value);
+//     }
+
+//     const handleSubmit = function(event){
+//         event.preventDefault();
+//         if (userInput){
+//             const dbRef = ref(realtime);
+//             push(dbRef, userInput);
+//             setUserInput("");
+//         }
+//         else{
+//             alert("I AM HUNGER SATIATE ME")
+//         }
+//     }
+    
+//     return (
+//         <div className="App">
+//             <h1>Readin'</h1>
+
+//             <form onSubmit={ handleSubmit }>
+//                 <label htmlFor="userBookChoice">add thy book ye fool </label>
+//                 <input 
+//                     type="text" 
+//                     id="userBookChoice"
+//                     onChange={ handleChange }
+//                     value={ userInput }
+//                     />
+//                 <button>feed data to database</button>
+//             </form>
+
+//             <ul>
+//                 {
+//                     bookList.map(function(individualBook){
+//                         return (
+//                         <li key={individualBook.key}>
+//                             <p>{individualBook.title}</p>
+//                         </li>
+//                         )
+//                     })
+//                 }
+//             </ul>
+//         </div>
+//     );
+// }
+
+// export default App;
 
 
 
